e73fd9475a0ccc2a9a8c30ecd8aa6486da43f7d4
timewarp180 <58781354+timewarp180@users.noreply.github.com>
2022-05-28 14:15:55 +0800

    new rep

---
+++ ChatApp/ChatApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp
+{
+    public partial class App : Application
+    {
+        public static float screenWidth { get; set; }
+        public static float screenHeight { get; set; }
+        public static float appScale { get; set; }
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new SplashPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Pages/ResetPassword.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResetPassword : ContentPage
+    {
+        public ResetPassword()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "Homepage");
+        }
+
+        private async void ResetPassword_Clicked(Object sender, EventArgs e)
+        {
+            FirebaseAuthResponseModel res = new FirebaseAuthResponseModel() { };
+            res = await DependencyService.Get<iFirebaseAuth>().ResetPassword(forgotemail.Text);
+
+            if (res.Status == true)
+            {
+                await DisplayAlert("Success", res.Response, "Okay");
+                await Navigation.PopModalAsync();
+                Application.Current.MainPage = new NavigationPage(new MainPage());
+            }
+            else
+            {
+                await DisplayAlert("Error", res.Response, "Okay");
+            }
+
+        }
+    }
+}
